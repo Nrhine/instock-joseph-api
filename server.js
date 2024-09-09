2a5a678638e1e97f67161fdef41f0247628d59c7
Angelo <yapangeloterezo@gmail.com>
2024-09-09 10:37:49 -0700

    nothing changed code-wise, just prettier formatting

--- server.js
+++ server.js
@@ -1,33 +1,32 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import itemRoutes from './routes/stock-routes.js';
-import warehouseRoutes from './routes/warehouse-routes.js';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import itemRoutes from "./routes/stock-routes.js";
+import warehouseRoutes from "./routes/warehouse-routes.js";
 
 const PORT = process.env.PORT || 8081;
 const app = express();
-
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
 // main route
-app.get('/', (req, res) => {
-  res.send('Welcome to the InStock API');
+app.get("/", (req, res) => {
+  res.send("Welcome to the InStock API");
 });
 
 // all inventory routes
-app.use('/api/inventories', itemRoutes);
+app.use("/api/inventories", itemRoutes);
 
 // all warehouse routes
-// app.use('/warehouses', warehouseRoutes);
+//app.use('/warehouses', warehouseRoutes);
 
 // post warehouse
 //app.use('/api', warehouseRoutes)
 
 // get inventories for a given warehouse /warehouses/:id/inventories
-app.use('/api/warehouses', warehouseRoutes);
+app.use("/api/warehouses", warehouseRoutes);
 
 app.listen(PORT, () => {
   console.log(`Running at http://localhost:${PORT}`);
